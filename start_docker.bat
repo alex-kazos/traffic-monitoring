3506c12ac383527550e6d26d681c2b7ea9cecf23
Alex <kazosalexandros@gmail.com>
2026-03-08 22:26:30 +0200

    isolate video tracking process

--- start_docker.bat
+++ start_docker.bat
@@ -1,6 +1,2 @@
-@echo off
-REM For Split
-docker build -t traffic-split -f Split/Dockerfile .
-
-REM For Tracking
-docker build -t traffic-tracking -f Tracking/Dockerfile .
+docker build -t traffic-monitor .
+docker run traffic-monitor
